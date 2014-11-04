2c2e05c723d9c2165f8ca0e73d6d316c10dbbce8
chunikuo <chuni.kuo@gmail.com>
2014-11-04 15:58:08 +0800

    Add: UART1 Task Control to detect input char
    USARTEventTask() can read char from UART1
    the max columns are 15 chars
    if type more than 15 chars, the 16-th char will be shown on next line.
    if press "enter" button , it will change next line.

--- CORTEX_M4F_STM32F4/main.c
+++ CORTEX_M4F_STM32F4/main.c
@@ -32,7 +32,7 @@
 #include <string.h>
 
 #include "main.h"
-#include "menu.h"
+//#include "menu.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -57,11 +57,111 @@ prvInit()
 	STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_GPIO );
 }
 
+void RCC_Configuration(void)
+{
+      /* --------------------------- System Clocks Configuration -----------------*/
+      /* USART1 clock enable */
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+      /* GPIOA clock enable */
+      RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+}
+
+void GPIO_Configuration(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    /*-------------------------- GPIO Configuration ----------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Connect USART pins to AF */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
+}
+
+void USART1_Configuration(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USARTx configuration ------------------------------------------------------*/
+    /* USARTx configured as follow:
+     *  - BaudRate = 9600 baud
+     *  - Word Length = 8 Bits
+     *  - One Stop Bit
+     *  - No parity
+     *  - Hardware flow control disabled (RTS and CTS signals)
+     *  - Receive and transmit enabled
+     */
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
+}
+
+void DrawUSART(uint8_t *t)
+{
+	LCD_SetTextColor(LCD_COLOR_RED);
+	LCD_DisplayStringLine(LCD_LINE_1, t);
+}
+
+void USARTEventTask()
+{
+    	RCC_Configuration();
+    	GPIO_Configuration();
+    	USART1_Configuration();
+
+	LCD_SetColors(LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+
+	char text[16];
+	int count_col = 0;
+	int count_line = 0;
+
+	while(1) 
+	{
+		while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+		char t = USART_ReceiveData(USART1);
+		
+		switch(t)
+		{
+			case '\r':
+				count_col = 0;
+				count_line++;
+				text[0] = '\0';
+			break;
+			default:
+				text[count_col++] = t;
+				text[count_col] = '\0';
+			break;
+		}
+			
+		LCD_DisplayStringLine(LINE(count_line), text);
+		if (count_col == 15)
+		{
+			count_col = 0;
+			count_line++;
+			text[0] = '\0';
+		}
+
+	}
+}
+
 //Main Function
 int main(void)
 {
 	prvInit();
 	
-	StartBulletTime();
+	//StartBulletTime();
+
+	xTaskCreate( USARTEventTask, "USART Event Task", 256, NULL, tskIDLE_PRIORITY + 2, NULL);
+	
+	vTaskStartScheduler();
 }
 
